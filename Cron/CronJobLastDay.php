5d5efb3ec4bddeaf725512d67317b7c42e06c763
Carol <91797824+CarolSilv@users.noreply.github.com>
2024-04-17 10:30:17 -0300

    Cron Finalizado

---
+++ Cron/CronJobLastDay.php
@@ -0,0 +1,17 @@
+<?php
+
+date_default_timezone_set('America/Sao_Paulo');
+
+$pasta = dir(__DIR__);
+
+$dataLimite = date("Y-m-d H:i:s",strtotime(date("Y-m-d H:i:s")."-1 day"));
+
+while (($arquivos = $pasta->read()) !== false) {
+    $files = new SplFileInfo($arquivos);
+    $dataArquivo = date("Y-m-d H:i:s", $files->getMTime());
+    if(strtotime($dataArquivo) <= strtotime($dataLimite)){
+        echo $dataArquivo . " menor que ". $dataLimite.PHP_EOL;
+        @unlink($arquivos);
+        echo $arquivos . " foi excluído".PHP_EOL;
+    }
+ }
